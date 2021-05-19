3278fb07b44cf299e429c9ba42ae32103d977fb1
eddicodes <edw.agyeman@gmail.com>
2021-05-19 15:20:47 +0200

    coingecko crypto API

--- src/App.js
+++ src/App.js
@@ -1,11 +1,23 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from "react";
+import "./App.css";
+import axios from "axios";
+
+// https://api.coingecko.com/api/v3/coins/markets?vs_currency=eur&order=market_cap_desc&per_page=100&page=1&sparkline=false
 
 function App() {
+  const [coins, setCoins] = useState([])
+
+  useEffect(() => {
+    axios.get('https://api.coingecko.com/api/v3/coins/markets?vs_currency=eur&order=market_cap_desc&per_page=100&page=1&sparkline=false')
+    .then(res => {
+      setCoins(res.data)
+      console.log(res.data)
+    })
+    .catch(error => console.log(error))
+  }, [])
+
   return (
-    <div className="App">
-      
-    </div>
+  <div className="App"></div>
   );
 }
 
