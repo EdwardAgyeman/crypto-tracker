4b5f458a3f29776b0cc5fee09e7437c0162fda5f
eddicodes <edw.agyeman@gmail.com>
2021-05-20 13:01:41 +0200

    Coin Component

--- src/App.js
+++ src/App.js
@@ -6,6 +6,7 @@ import axios from "axios";
 
 function App() {
   const [coins, setCoins] = useState([])
+  const [search, setSearch] = useState('')
 
   useEffect(() => {
     axios.get('https://api.coingecko.com/api/v3/coins/markets?vs_currency=eur&order=market_cap_desc&per_page=100&page=1&sparkline=false')
@@ -16,8 +17,19 @@ function App() {
     .catch(error => console.log(error))
   }, [])
 
+  const handleChange = e => {
+    setSearch(e.target.value)
+  }
+
   return (
-  <div className="App"></div>
+  <div className="coin-app">
+    <div className="coin-search">
+      <h1 className="coin-text">Search a currency</h1>
+      <form >
+        <input type="text" placeholder="Search" className="coin-input" onChange={handleChange} />
+      </form>
+    </div>
+  </div>
   );
 }
 
